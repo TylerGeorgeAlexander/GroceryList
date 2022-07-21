e8b0d6777baa75c5f564b1a5cf087b6f77a7cb61
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-07-21 13:15:05 -0700

    testing branches

--- src/Content.js
+++ src/Content.js
@@ -2,13 +2,30 @@ import React from "react";
 
 const Content = () => {
   const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave", "Laura", "Laura"];
+    const names = ["Laura", "Lauraverb", "Tycee", "Lauraverb", "Lauraverb"];
     const int = Math.floor(Math.random() * 5);
+
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`I know: ${name}`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
       <p>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2('Laura')}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
